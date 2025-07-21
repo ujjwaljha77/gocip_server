e45037813c51b3e487ebe889f4172c9a33aed99c
ujjwaljha77 <jhaujjwal1970@gmail.com>
2025-07-21 18:48:41 +0530

    updated

---
+++ api/index.js
@@ -0,0 +1,31 @@
+// api/index.js
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "../config/db.js";
+import authRoutes from "../routes/authRoute.js";
+import categoryRoutes from "../routes/categoryRoutes.js";
+import productRoutes from "../routes/productRoutes.js";
+import cors from "cors";
+
+// configure env
+dotenv.config();
+
+// connect DB
+connectDB();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+// export handler for vercel
+export default app;
